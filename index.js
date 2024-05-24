76e3b1bbba5628d8a266ade7db64b8b1f81cff3c
mengaww2ya <mengaww2ya@gmail.com>
2024-05-24 23:45:41 +0300

    css,html,js file pushed

---
+++ index.js
@@ -0,0 +1,86 @@
+function introductionFun() {
+  const inroduction = document.querySelector('.introduction');
+  const observation = document.querySelector('.observation');
+  const suggesion = document.querySelector('.suggesion');
+  const sumery = document.querySelector('.sumery');
+  const goodby = document.querySelector('.goodby');
+  const firstContinuebutton = document.querySelector('.first_continue_button');
+
+  inroduction.style.display = 'block';
+  observation.style.display = 'none';
+  suggesion.style.display = 'none';
+  sumery.style.display = 'none';
+  goodby.style.display = 'none';
+  firstContinuebutton.style.display = 'none';
+}
+function observationFun() {
+  const inroduction = document.querySelector('.introduction');
+  const observation = document.querySelector('.observation');
+  const suggesion = document.querySelector('.suggesion');
+  const sumery = document.querySelector('.sumery');
+  const goodby = document.querySelector('.goodby');
+
+  inroduction.style.display = 'none';
+  observation.style.display = 'block';
+  suggesion.style.display = 'none';
+  sumery.style.display = 'none';
+  goodby.style.display = 'none';
+}
+function suggesionFun() {
+  const inroduction = document.querySelector('.introduction');
+  const observation = document.querySelector('.observation');
+  const suggesion = document.querySelector('.suggesion');
+  const sumery = document.querySelector('.sumery');
+  const goodby = document.querySelector('.goodby');
+
+  inroduction.style.display = 'none';
+  observation.style.display = 'none';
+  suggesion.style.display = 'block';
+  sumery.style.display = 'none';
+  goodby.style.display = 'none';
+}
+function sumeryFun() {
+  const inroduction = document.querySelector('.introduction');
+  const observation = document.querySelector('.observation');
+  const suggesion = document.querySelector('.suggesion');
+  const sumery = document.querySelector('.sumery');
+  const goodby = document.querySelector('.goodby');
+
+  inroduction.style.display = 'none';
+  observation.style.display = 'none';
+  suggesion.style.display = 'none';
+  sumery.style.display = 'block';
+  goodby.style.display = 'none';
+}
+function goodbyFun() {
+  const inroduction = document.querySelector('.introduction');
+  const observation = document.querySelector('.observation');
+  const suggesion = document.querySelector('.suggesion');
+  const sumery = document.querySelector('.sumery');
+  const goodby = document.querySelector('.goodby');
+
+  inroduction.style.display = 'none';
+  observation.style.display = 'none';
+  suggesion.style.display = 'none';
+  sumery.style.display = 'none';
+  goodby.style.display = 'block';
+}
+function allnonFun() {
+  const inroduction = document.querySelector('.introduction');
+  const observation = document.querySelector('.observation');
+  const suggesion = document.querySelector('.suggesion');
+  const sumery = document.querySelector('.sumery');
+  const goodby = document.querySelector('.goodby');
+
+  inroduction.style.display = 'none';
+  observation.style.display = 'none';
+  suggesion.style.display = 'none';
+  sumery.style.display = 'none';
+  goodby.style.display = 'none';
+}
+goodbyFun();
+sumeryFun();
+suggesionFun();
+observationFun();
+introductionFun();
+allnonFun();
